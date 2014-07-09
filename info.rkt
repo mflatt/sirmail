1cac6bc0a3539e89a95086e3f21b1fb0054c8073
Matthew Flatt <mflatt@racket-lang.org>
2014-07-09 07:21:45 +0100

    declare dependencies

--- info.rkt
+++ info.rkt
@@ -1,3 +1,11 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps '("base"
+               "compatibility-lib"
+               "drracket" ; for browser/htmltext
+               "gui-lib"
+               "net-lib"
+               "parser-tools-lib"
+               "scheme-lib"))
